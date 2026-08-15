aedd6a19be7d2ad4f719a73ce8e9ea044643867a
leolatri <kucka3003@gmail.com>
2026-08-15 19:01:55 +0300

    feat(object categories): create endpoints for object categoties table

---
+++ backend/src/objectCategories/controller.ts
@@ -0,0 +1,103 @@
+import { Body, ConflictException, Controller, Delete, Get, NotFoundException, Param, ParseIntPipe, Patch, Post } from "@nestjs/common";
+import { DatabaseError, QueryResultRow } from "pg";
+import { DatabaseService } from "../database/service";
+import { ObjCategoriesDTO } from "../dto/dtoModels";
+
+interface ObjCategoriesModel extends QueryResultRow {
+    id: number,
+    name: string
+}
+
+@Controller('object-categories')
+export class ObjCategoriesController {
+    constructor(private readonly dbService: DatabaseService) { }
+
+    @Get()
+    async getAll(): Promise<ObjCategoriesModel[]> {
+        const result = await this.dbService.query<ObjCategoriesModel>(
+            `SELECT id, name
+            FROM object_categories
+            ORDER BY id
+            `
+        );
+
+        return result.rows;
+    };
+
+    @Get(':id')
+    async getObjectCategoriesById(@Param('id', ParseIntPipe) id: number): Promise<ObjCategoriesModel> {
+        const result = await this.dbService.query<ObjCategoriesModel>(
+            `SELECT id, name
+            FROM object_categories
+            WHERE id = $1
+            `, [id]
+        );
+        const object = result.rows[0];
+
+        if (!object) throw new NotFoundException(`Not found problem object by id = ${id}`);
+
+        return object;
+    };
+
+    @Post()
+    async createObjectCategories(
+        @Body() dto: ObjCategoriesDTO,
+    ): Promise<ObjCategoriesModel> {
+        try {
+            const result = await this.dbService.query<ObjCategoriesModel>(
+                `INSERT INTO object_categories (name)
+                VALUES ($1)
+                RETURNING id, name`, [dto.name]
+            );
+
+            return result.rows[0];
+        } catch (error: unknown) {
+            if (error instanceof DatabaseError && error.code === '23505') {
+                throw new ConflictException(`Problem object "${dto.name}" already exists`);
+            }
+            throw error;
+        }
+    }
+
+    @Patch(':id')
+    async updateObjectCategories(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() dto: ObjCategoriesDTO
+    ): Promise<ObjCategoriesModel> {
+        const result = await this.dbService.query<ObjCategoriesModel>(
+            `UPDATE object_categories
+            SET
+                name = $1,
+                updated_at = NOW()
+            WHERE id = $2
+            RETURNING id, name
+            `, [dto.name, id]
+        );
+
+        const object = result.rows[0];
+
+        if (!object) throw new NotFoundException(`Cannot update problem object whith id = ${id}`);
+
+        return object;
+    }
+
+    @Delete(':id')
+    async deleteObjectCategories(@Param('id', ParseIntPipe) id: number): Promise<ObjCategoriesModel> {
+        const result = await this.dbService.query<ObjCategoriesModel>(
+            `DELETE FROM object_categories
+            WHERE id = $1
+            RETURNING id, name
+            `, [id]
+        );
+
+        const object = result.rows[0];
+
+        if (!object) {
+            throw new NotFoundException(
+                `Problem object with id = ${id} not found`,
+            );
+        }
+
+        return object;
+    }
+}
---
+++ backend/src/objectCategories/module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { DatabaseModule } from "../database/module";
+import { ObjCategoriesController } from "./controller";
+
+@Module({
+    imports: [DatabaseModule],
+    controllers: [ObjCategoriesController],
+})
+
+export class ObjCategoriesModule {};
