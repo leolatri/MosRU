24506783e9621dd8682ccdd5486f4eb5760a7cf1
leolatri <kucka3003@gmail.com>
2026-08-19 13:45:33 +0300

    feat(frontend): add button for exit

--- frontend/src/components/layout/LayoutMenu.tsx
+++ frontend/src/components/layout/LayoutMenu.tsx
@@ -1,25 +1,38 @@
-import { Image, Layout, Menu } from 'antd';
+import { Button, Image, Layout, Menu } from 'antd';
 import st from './style.module.scss';
-import { Content, Header } from "antd/es/layout/layout";
 import logo from '../../assets/logo.svg';
-import { ProfileOutlined  , BarChartOutlined  } from '@ant-design/icons';
-import { Outlet } from 'react-router';
+import { ProfileOutlined, BarChartOutlined } from '@ant-design/icons';
+import { Outlet, useNavigate } from 'react-router';
 import type { MenuProps } from 'antd/lib/menu';
+import { LogoutOutlined } from '@ant-design/icons';
+import { Content, Header } from "antd/es/layout/layout";
 
 const LayoutMenu = () => {
+    const navigate = useNavigate();
     const menuItems: MenuProps['items'] = [
-        { key: '/violations', icon: <ProfileOutlined/>, label: 'Нарушения' },
-        { key: '/dashboard', icon: <BarChartOutlined  /> , label: 'Дашборд' },
+        { key: '/violations', icon: <ProfileOutlined />, label: 'Нарушения' },
+        { key: '/dashboard', icon: <BarChartOutlined />, label: 'Дашборд' },
     ];
+
+    const handleExit = () => {
+        navigate('/login');
+    };
+
     return (
         <Layout className={st.layout}>
             <Header className={st.layout__header}>
-                <Image
-                    src={logo}
-                    width={60}
-                    preview={false}
-                    className={st.layout__img}
-                />
+                <div className={st.layout__block}>
+                    <Image
+                        src={logo}
+                        width={60}
+                        preview={false}
+                        className={st.layout__img}
+                    />
+                    <Button
+                        icon={<LogoutOutlined />}
+                        onClick={handleExit}
+                    >Выход</Button>
+                </div>
                 <Menu
                     theme="light"
                     mode="horizontal"
@@ -28,7 +41,7 @@ const LayoutMenu = () => {
                 />
             </Header>
             <Content className={st.layout__content}>
-                <Outlet/>
+                <Outlet />
             </Content>
         </Layout>
     )
